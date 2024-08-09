d87ccc8965b9b32d2a8cbea682762ca69145a6af
David gomez <david.gomez04@hotmail.com>
2024-08-09 16:13:21 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Nav from './components/nav'
+
+function App() {
+
+  return (
+    <main className=''>
+      <div className='my-8 ml-5'>
+      <Nav></Nav>
+      </div>
+    
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/nav.jsx
@@ -0,0 +1,13 @@
+import logo from "../../images/logo.svg"
+
+function Nav() {
+    return ( 
+        <>
+        <nav className=" ">
+            <img src={logo} alt="logo"/>
+        </nav>
+        </>
+     );
+}
+
+export default Nav;
