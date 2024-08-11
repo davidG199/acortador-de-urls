1b79f2561b0e55900753d4fdc6e25b8c180acc59
David gomez <david.gomez04@hotmail.com>
2024-08-10 22:29:29 -0500

    nuevos estilos y ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 import "./App.css";
+import Boost from "./components/boost";
+import Footer from "./components/footer";
 import Hero from "./components/hero";
+import Info from "./components/info";
 import InputLink from "./components/inputLink";
 import Nav from "./components/nav";
 
 function App() {
   return (
-    <main className="">
-      <div className="my-8 mx-4 bg-white">
-        <Nav />
-        <Hero />
-        <InputLink />
-      </div>
-      <div className="my-8 mx-4 bg-[var(--Gray)]">
-      </div>
-    </main>
+    <>
+      <main>
+        <section className="mt-8 mx-5 bg-white">
+          <Nav />
+          <Hero />
+          <InputLink />
+        </section>
+        <section className="bg-gray-200 pb-16">
+          <div className="mx-5">
+            <div className="text-center my-16">
+              <p className="font-bold text-[var(--Very-Dark-Violet)] text-2xl">
+                Advanced Statistics
+              </p>
+              <p className=" text-sm text-[var(--Grayish-Violet)] mt-5 leading-6">
+                Track how your links are performing across the web with our
+                advanced statistics dashboard.
+              </p>
+            </div>
+            <Info />
+          </div>
+        </section>
+        <Boost />
+      </main>
+      <footer className="bg-[var(--Very-Dark-Violet)] py-10">
+        <Footer/>
+      </footer>
+    </>
   );
 }
 
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,15 +2,17 @@ import heroIcon from "../../images/illustration-working.svg";
 
 function Hero() {
   return (
-    <div className="text-center">
-        <img src={heroIcon} alt="heroIcon" className="h-[300px] relative left-10" />
-      <div className="mt-8">
+    <div className="text-center grid grid-cols-1 md:grid-cols-2">
+      <picture className="mt-5 md:order-2">
+        <img src={heroIcon} alt="heroIcon" className="h-[310px]" />
+      </picture>  
+      <div className="mt-8 md:text-left md:order-1">
         <h1 className=" text-4xl font-bold text-[var(--Very-Dark-Blue)]">More than just shorter links</h1>
         <p className="text-[var(--Grayish-Violet)] mt-3 text-base">
           Build your brand's recognition and get detailed insights on how your
           links are performing.
         </p>
-        <button className="mt-7 bg-[var(--Cyan)] px-8 py-3 text-white font-bold rounded-3xl">Get Started</button>
+        <button className="mt-7 bg-[var(--Cyan)] px-8 py-3 text-white font-bold rounded-full">Get Started</button>
       </div>
     </div>
   );
