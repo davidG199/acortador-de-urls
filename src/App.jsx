37b3d8bb7d31be8f66b0bd485d413ae0df3ae9da
David gomez <david.gomez04@hotmail.com>
2024-09-15 15:29:05 -0500

    correcion de la logica de la api

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Boost from "./components/boost";
 import Footer from "./components/footer";
 import Hero from "./components/hero";
 import Info from "./components/info";
 import InputLink from "./components/inputLink";
+import Link from "./components/link";
 import Nav from "./components/nav";
 
 function App() {
+  const [links, setLinks] = useState([]);
+
+  // Cargar los links de localStorage al montar el componente
+  useEffect(() => {
+    const storedLinks =
+      JSON.parse(localStorage.getItem("shortenedLinks")) || [];
+    setLinks(storedLinks);
+  }, []);
+
+  // Callback para actualizar los enlaces después de acortar uno
+  const addNewLink = (newLink) => {
+    const updatedLinks = [newLink, ...links];
+    setLinks(updatedLinks);
+    localStorage.setItem("shortenedLinks", JSON.stringify(updatedLinks));
+  };
+
   return (
     <>
       <main>
-        <section className="mt-8 mx-5 bg-white">
+        <section className="my-8 mx-5  bg-white">
           <Nav />
           <Hero />
-          <InputLink />
+          <InputLink onNewLink={addNewLink} />{" "}  {/* mandamos el link a la funcion para actualizar los enlaces del localStorage */}
+          <Link links={links} />
         </section>
         <section className="bg-gray-200 pb-16">
           <div className="mx-5">
--- src/components/inputLink.jsx
+++ src/components/inputLink.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 
-function InputLink() {
+function InputLink({ onNewLink }) {
   const [inputText, setInputText] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (!inputText) {
-      alert("Input is empty");
+      alert("debes llenar el campo");
       return;
     } else {
       try {
-        //enviar la petición al endpoint
+        //enviar la petición a la api
         const response = await fetch(
+          //utilizo un de terceros proxy para evitar el problema de cors y codificamos la url
           "https://corsproxy.io/?" +
             encodeURIComponent("https://cleanuri.com/api/v1/shorten"),
           {
@@ -27,10 +28,25 @@ function InputLink() {
         );
 
         if (response.ok) {
+          //recibimos la respuesta de la api
           const data = await response.json();
           console.log("URL acortada:", data.result_url);
+          if (data.result_url) {
+            //si nuestra respuesta contiene la url, creamos un nuevo objeto 
+            const newLink = {
+              originalUrl: inputText,
+              shortenedUrl: data.result_url,
+            };
+            //llamamos al callback para agregar el nuevo link al componente padre
+            onNewLink(newLink);
+            //limpar el input
+            setInputText("");
+          } else {
+            console.log("No se pudo acortar la url");
+          }
         } else {
-          console.log("Error en la solicitud");
+          const errorData = await response.json();
+          console.log("Error en la solicitud", errorData.error);
         }
       } catch (error) {
         console.error("Error en la petición:", error);
@@ -40,7 +56,7 @@ function InputLink() {
 
   return (
     <div className="mt-20 p-6 bg-[var(--Dark-Violet)] bg-[url('../../images/bg-shorten-mobile.svg')] bg-right-top bg-no-repeat rounded-lg">
-      <div className="flex flex-col ">
+      <div className="flex flex-col gap-4 ">
         <input
           type="url"
           placeholder="Shorten a link here..."
