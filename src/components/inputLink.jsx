0887e09ae4a1d5e0c038b185048d10bea928146c
David gomez <david.gomez04@hotmail.com>
2024-09-16 19:46:05 -0500

    validacion de input con estilos

--- src/components/inputLink.jsx
+++ src/components/inputLink.jsx
@@ -2,18 +2,19 @@ import { useState } from "react";
 
 function InputLink({ onNewLink }) {
   const [inputText, setInputText] = useState("");
+  const [errorInput, setErrorInput] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!inputText) {
-      alert("debes llenar el campo");
+    if (!inputText.trim()) {
+      setErrorInput(true);
       return;
     } else {
       try {
         //enviar la petición a la api
         const response = await fetch(
-          //utilizo un de terceros proxy para evitar el problema de cors y codificamos la url
+          //utilizo un proxy de terceros para evitar el problema de cors y codificamos la url
           "https://corsproxy.io/?" +
             encodeURIComponent("https://cleanuri.com/api/v1/shorten"),
           {
@@ -32,7 +33,7 @@ function InputLink({ onNewLink }) {
           const data = await response.json();
           console.log("URL acortada:", data.result_url);
           if (data.result_url) {
-            //si nuestra respuesta contiene la url, creamos un nuevo objeto 
+            //si nuestra respuesta contiene la url, creamos un nuevo objeto
             const newLink = {
               originalUrl: inputText,
               shortenedUrl: data.result_url,
@@ -41,6 +42,7 @@ function InputLink({ onNewLink }) {
             onNewLink(newLink);
             //limpar el input
             setInputText("");
+            setErrorInput(false);
           } else {
             console.log("No se pudo acortar la url");
           }
@@ -55,17 +57,30 @@ function InputLink({ onNewLink }) {
   };
 
   return (
-    <div className="mt-20 p-6 bg-[var(--Dark-Violet)] bg-[url('../../images/bg-shorten-mobile.svg')] bg-right-top bg-no-repeat rounded-lg">
-      <div className="flex flex-col gap-4 ">
-        <input
-          type="url"
-          placeholder="Shorten a link here..."
-          className="py-3 px-2 rounded-lg text-base"
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-        />
+    <div className="input-link mt-20 p-6 md:px-10 md:pt-10  bg-[var(--Dark-Violet)]  rounded-lg">
+      <div className="flex flex-col md:flex-row md:items-start md:justify-between md:gap-4 w-full">
+        <div className="flex-1 flex flex-col">
+          <input
+            type="url"
+            placeholder="Shorten a link here..."
+            className={`w-full py-3 px-5 rounded-lg text-base md:flex-1 md:py-4 ${
+              errorInput ? " border-2 border-red-500 placeholder:text-red-500/50" : ""
+            }`}
+            value={inputText}
+            onChange={(e) => {
+              setInputText(e.target.value);
+              setErrorInput(false);
+            }}
+          />
+          <span className=" md:min-h-7">
+            {errorInput && (
+              <p className="text-red-500 text-[14px] mt-2 italic">Please add a link</p>
+            )}
+          </span>
+        </div>
+
         <button
-          className="mt-5 text-white bg-[var(--Cyan)] py-2 font-bold rounded-lg"
+          className="mt-3 text-white bg-[var(--Cyan)] py-2 font-bold rounded-md md:py-4 md:px-8 md:mt-0 cursor-pointer hover:bg-[var(--Cyan-hover)]"
           type="submit"
           onClick={handleSubmit}
         >
